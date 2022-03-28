f3266d00237cdbc7f885766ba66a0cbf2e91e97a
Jegor <georg.reichert.1998@gmail.com>
2022-03-28 16:13:27 +0200

    Kleinere Anpassungen + Controller Test

--- Software Project/1 - Backend/0-cleanproject-plugins/src/main/java/de/dhbw/ase/theone/TheOneApplication.java
+++ Software Project/1 - Backend/0-cleanproject-plugins/src/main/java/de/dhbw/ase/theone/TheOneApplication.java
@@ -1,9 +1,21 @@
 package de.dhbw.ase.theone;
 
+import de.dhbw.ase.theone.collection.Collection;
+import de.dhbw.ase.theone.collection.CollectionRepository;
 import de.dhbw.ase.theone.country.Country;
 import de.dhbw.ase.theone.country.CountryRepository;
 import de.dhbw.ase.theone.manufacturer.Manufacturer;
 import de.dhbw.ase.theone.manufacturer.ManufacturerRepository;
+import de.dhbw.ase.theone.note.types.BaseNote;
+import de.dhbw.ase.theone.note.types.HeadNote;
+import de.dhbw.ase.theone.note.types.HeartNote;
+import de.dhbw.ase.theone.perfume.Perfume;
+import de.dhbw.ase.theone.perfume.PerfumeRepository;
+import de.dhbw.ase.theone.note.NoteRepository;
+import de.dhbw.ase.theone.rating.Rating;
+import de.dhbw.ase.theone.rating.RatingRepository;
+import de.dhbw.ase.theone.wishlist.Wishlist;
+import de.dhbw.ase.theone.wishlist.WishlistRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -23,7 +35,15 @@ public class TheOneApplication {
     }
 
     @Bean
-    public CommandLineRunner demo(CountryRepository countryRepository,ManufacturerRepository manufacturerRepository) {
+    public CommandLineRunner demo(
+            CountryRepository countryRepository,
+            ManufacturerRepository manufacturerRepository,
+            NoteRepository noteRepository,
+            PerfumeRepository perfumeRepository,
+            CollectionRepository collectionRepository,
+            WishlistRepository wishlistRepository,
+            RatingRepository ratingRepository
+    ) {
         return (args) -> {
             Country ukraine = new Country("UKR","Ukraine");
             Country germany = new Country("GER","Deutschland");
@@ -35,6 +55,39 @@ public class TheOneApplication {
 
             Manufacturer testManufacturer = new Manufacturer("Testhersteller",ukraine);
             manufacturerRepository.save(testManufacturer);
+
+            BaseNote baseNote = new BaseNote("BaseNote","Basisnote");
+            noteRepository.save(baseNote);
+            HeartNote heartNote = new HeartNote("HeartNote","Herznote");
+            noteRepository.save(heartNote);
+            HeadNote headNote = new HeadNote("HeadNote","Kopfnote");
+            noteRepository.save(headNote);
+
+            Collection collection = new Collection("Neue Collection");
+            collectionRepository.save(collection);
+
+            Wishlist wishList = new Wishlist("Neue Wishlist");
+            wishlistRepository.save(wishList);
+
+            Perfume perfume = new Perfume("JegorOne",1998,testManufacturer,350,baseNote,heartNote,headNote);
+            perfumeRepository.save(perfume);
+
+            Perfume perfume2 = new Perfume("PaddyTwo",2000,testManufacturer,350,baseNote,heartNote,headNote);
+            perfumeRepository.save(perfume2);
+
+            Perfume perfume3 = new Perfume("DaveThree",2001,testManufacturer,350,baseNote,heartNote,headNote);
+            perfumeRepository.save(perfume3);
+
+            collection.addPerfume(perfume);
+            collection.addPerfume(perfume2);
+            collectionRepository.save(collection);
+
+            wishList.addPerfume(perfume);
+            wishList.addPerfume(perfume3);
+            wishlistRepository.save(wishList);
+
+            Rating rating = new Rating(perfume,"Georg Reichert",6,6,6,6,8);
+            ratingRepository.save(rating);
         };
     }
 
---
+++ Software Project/1 - Backend/0-cleanproject-plugins/src/main/test/de/dhbw/ase/theone/plugins/rest/country/CountryControllerTest.java
@@ -0,0 +1,69 @@
+package de.dhbw.ase.theone.plugins.rest.country;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.dhbw.ase.theone.country.Country;
+import de.dhbw.ase.theone.country.CountryResource;
+import de.dhbw.ase.theone.services.country.CountryApplicationService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = de.dhbw.ase.theone.TheOneApplication.class)
+@AutoConfigureMockMvc
+class CountryControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    CountryController countryController;
+
+    @MockBean
+    CountryApplicationService countryApplicationService;
+
+    private Country country;
+    private Country secondCountry;
+
+    @BeforeEach
+    void setUp() {
+        country = new Country(1L,"UKR", "Ukraine");
+        secondCountry = new Country(2L,"D", "Deutschland");
+    }
+
+    @Test
+    void getCountries() throws Exception {
+        List<Country> countries = Arrays.asList(country, secondCountry);
+        doReturn(countries).when(this.countryController).getCountries();
+        MvcResult mvcResult = this.mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/country")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        String responseBody = mvcResult.getResponse().getContentAsString();
+        List<Country> readCountries = Arrays.asList(this.objectMapper.reader().forType(Country[].class).readValue(responseBody));
+
+        assertEquals(countries.get(0).getAbbreviation(), readCountries.get(0).getAbbreviation());
+        assertEquals(countries.get(0).getName(), readCountries.get(0).getName());
+
+        assertEquals(countries.get(1).getAbbreviation(), readCountries.get(1).getAbbreviation());
+        assertEquals(countries.get(1).getName(), readCountries.get(1).getName());
+
+
+    }
+}
